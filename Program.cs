55b1ec4c253ccb73c9e800a7f1d98fc60c99f501
ilja.tihhonov <ilja.tihhonov@outlook.com>
2022-04-14 13:49:07 +0300

    CalculateSumInLoop

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CalculateSumInLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int sum = 0;
+
+            for (int i = 1; i < 11; i++)
+            {
+                int myRandom = rnd.Next(1, 11);
+                sum = sum + myRandom;
+                Console.WriteLine($"{i}. random number: {myRandom}");
+            }
+            Console.WriteLine($"Sum is = {sum}", sum);
+
+
+        }
+
+    }
+}
